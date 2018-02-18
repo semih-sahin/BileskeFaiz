8ff78a1a2e81a4148ba4e3e4b90fa12065efb016
Semih Sahin <ckl.mhnds@gmail.com>
2018-02-18 18:49:29 +0300

    Bileske-Faiz-yuzde-on

---
+++ BileskeFaiz.cpp
@@ -0,0 +1,25 @@
+/*
+	Bileske faiz ile toplam para hesaplama (BileskeFaiz.cpp) - Semih Sahin
+*/
+#include <iostream>
+#include <conio.h>
+#include <locale.h>
+using namespace std;
+int main(){
+	setlocale(LC_ALL,"turkish");
+	double toplamPara=0;
+	int para, ay;
+//
+	cout << "Paranýzý giriniz : ";
+	cin >> para;
+	cout << "Paranýz kaç ay beklesin ?" << endl;
+	cin >> ay;
+	toplamPara = para;
+//
+	for (int i=1; i<=ay; i++){
+		toplamPara += toplamPara/10;
+	}
+	cout << "Toplam para : " << toplamPara << endl;
+	getch();
+	return 0;
+}
